7d9e8f99e00f23e8d1f618582850955e899b4d57
Mariyan Yordanov <marsrewq@gmail.com>
2023-11-23 01:21:43 +0200

    aad a few new problems

--- 03.Objects-and-Classes.js
+++ 03.Objects-and-Classes.js
@@ -116,4 +116,78 @@ function convertObjectToJSON(firstName, lastName, hairColor) {
     console.log(stringifyPerson);
 }
 
-convertObjectToJSON("Ana", "O`Reilly", "Ginger");
+//convertObjectToJSON("Ana", "O`Reilly", "Ginger");
+
+/*
+5. Phone Book
+
+Write a function that stores information about a person’s name and phone number. The input is an array of strings with space-separated name and number. Replace duplicate names. Print the result as shown. 
+*/
+
+function phoneBook(array) {
+
+    let phoneBook = {};
+
+    for (const element of array) {
+        const [name, phone] = element.split(' ');
+        if (!phoneBook.hasOwnProperty(name)) {
+            phoneBook[name] = phone;
+        }
+    }
+
+    for (const key in phoneBook) {
+        console.log(`${key} -> ${phoneBook[key]}`);
+    }
+}
+
+//phoneBook(['Tim 0834212554', 'Peter 0877547887', 'Bill 0896543112', 'Tim 0876566344']);
+
+
+/*
+6. Meetings
+
+Write a function that manages meeting appointments. The input comes as an array of strings. Each string contains a weekday and person’s name. For each successful meeting, print a message. If you receive the same weekday twice, the meeting cannot be scheduled so print a conflicting message. In the end, print a list of all successful meetings.
+*/
+
+function meetings(array) {
+    const meeting = {};
+    for (const element of array) {
+        const [day, name] = element.split(' ');
+        if (!meeting.hasOwnProperty(day)) {
+            meeting[day] = name;
+            console.log(`Scheduled for ${day}`);
+        } else {
+            console.log(`Conflict on ${day}!`);
+        }
+    }
+
+    Object.keys(meeting).forEach((day) => {
+        console.log(`${day} -> ${meeting[day]}`);
+    })
+}
+
+//meetings(['Monday Peter', 'Wednesday Bill', 'Monday Tim', 'Friday Tim']);
+
+/*
+7. Address Book
+
+Write a function that stores information about a person’s name and his address. The input comes as an array of strings. Each string contains the name and the address separated by a colon. If you receive the same name twice just replace the address. In the end, print the full list, sorted alphabetically by the person’s name.
+*/
+
+function getAddresses(input) {
+    const addressBook = {}
+
+    for (const person of input) {
+        const [name, address] = person.split(':');
+        addressBook[name] = address;
+    }
+
+    const peopleNames = Object.entries(addressBook);
+    const sortedPeople = peopleNames.sort();
+
+    for (const key of sortedPeople) {
+        console.log(`${key[0]} -> ${key[1]}`);
+    }
+}
+
+getAddresses(['Bob:Huxley Rd', 'John:Milwaukee Crossing', 'Peter:Fordem Ave', 'Bob:Redwing Ave', 'George:Mesta Crossing', 'Ted:Gateway Way', 'Bill:Gateway Way', 'John:Grover Rd', 'Peter:Huxley Rd', 'Jeff:Gateway Way', 'Jeff:Huxley Rd']);
