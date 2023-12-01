f920b3c05ca2435674749f927e700bcb223ef752
Mariyan Yordanov <marsrewq@gmail.com>
2023-12-01 05:14:09 +0200

    add more tasks

--- 05.JS-Front-End-DOM-Introduction-Exercises/07. Hell's Kitchen/app.js
+++ 05.JS-Front-End-DOM-Introduction-Exercises/07. Hell's Kitchen/app.js
@@ -3,15 +3,10 @@
 
 You will be given an array of strings, which represents a list of all the restaurants with their workers.
 When the [Send] button is clicked:
-
 · Display the best restaurant of all the added restaurants with its average salary and best salary.
-
 · If there is a restaurant in the input array that is added more than once, you need to add new workers to the old ones and update the values of the average salary and the best salary.
-
 · The best restaurant is the restaurant with the highest average salary. If two restaurants have the same average salary the best restaurant is the first one added.
-
 · Display all workers in the best restaurant with their salaries.
-
 The best restaurant's workers should be sorted by their salaries in descending order.
 
 Input
@@ -22,6 +17,7 @@ Output
 
 · The output contains two strings
 - The first one is the best restaurant in the format:
+
 `Name: {restaurant name} Average Salary: {restaurant avgSalary} Best Salary: {restaurant bestSalary}`
 
 avgSalary and bestSalary must be formatted to the second decimal point.
@@ -32,12 +28,67 @@ Constraints
 
 · The workers will be always unique
 */
-
+"use strict"
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
+   //["PizzaHut - Peter 500, George 300, Mark 800","TheLake - Bob 1300, Jane 660, Joe 780"]
+   let result = document.getElementById("btnSend");
+   let input = Array.from(document.querySelectorAll("#inputs > textarea"));
 
-   function onClick () {
-      
-      
+   function onClick() {
+
+      let restorants = [];
+
+      for (let i = 0; i < input.length; i++) {
+         const element = input[i].value;
+
+         let sumSalary = 0;
+         let workerCount = 0;
+
+         let restorant = {
+            restorantName: "",
+            workers: {},
+            averageSalary: 0,
+            bestSalary: 0,
+         };
+
+         let restorantInfo = element.split(" - ");
+         restorant.restorantName = restorantInfo[0];
+
+         let workersInfo = restorantInfo[1].split(", ")
+
+         for (const info of workersInfo) {
+
+            const [name, salary] = info.split(" ");
+            let salaryInNumber = Number(salary);
+
+            if (salaryInNumber > restorant.bestSalary) {
+               restorant.bestSalary = salaryInNumber;
+            }
+
+            if (!restorant.workers.hasOwnProperty(name)) {
+               restorant.workers[name] = salaryInNumber;
+               workerCount++;
+            } else {
+               restorant.workers[name] += salaryInNumber;
+            }
+
+            sumSalary += salaryInNumber;
+         }
+
+         restorant.averageSalary = sumSalary / workerCount;
+         restorants.push(restorant);
+      }
+
+      let theBestRestorant = restorants.sort((a, b) => b.averageSalary - a.averageSalary)[0];
+
+      console.log(`Name: ${theBestRestorant.restorantName} Average Salary: ${theBestRestorant.averageSalary} Best Salary: ${theBestRestorant.bestSalary}`);
+
+      let sortedWorkers = Object.entries(theBestRestorant.workers).sort((a, b) => b.salary - a.salary);
+
+      for (const [worker, salary] of sortedWorkers) {
+         console.log(`Name: ${worker} With Salary: ${salary}`);
+      }
    }
+
 }
--- 05.JS-Front-End-DOM-Introduction-Exercises/08. Generate Report/report.js
+++ 05.JS-Front-End-DOM-Introduction-Exercises/08. Generate Report/report.js
@@ -1,3 +1,35 @@
 function generateReport() {
-    //TODO
+    let personObject = {};
+    let personAllObjects = [];
+    let columnNumbers = [];
+
+    let columnsAll = document.querySelectorAll("thead tr th input");
+    console.log(columnsAll);
+
+    for (let i = 0; i < columnsAll.length; i++) {
+
+        const checkboxState = columnsAll[i].checked;
+
+        if (checkboxState) {
+            columnNumbers.push(i);
+        }
+    }
+
+    let columnTitels = document.querySelectorAll("thead tr")[0].getElementsByTagName("th");
+    let rowsCount = document.querySelectorAll("tbody tr").length;
+
+    for (let row = 0; row < rowsCount; row++) {
+
+        columnNumbers.forEach((element) => {
+
+            let key = columnTitels[element].textContent.trim().toLowerCase();
+            let value = document.querySelectorAll("tbody tr")[row].getElementsByTagName("td")[element].textContent;
+            personObject[key] = value;
+        });
+
+        personAllObjects.push(Object.assign(personObject));
+        personObject = {};
+    }
+
+    document.getElementById("output").innerHTML = JSON.stringify(personAllObjects);
 }
