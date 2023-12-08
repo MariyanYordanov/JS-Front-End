92aed74821204bb27107e4defa1aff2e1df7016c
Mariyan Yordanov <marsrewq@gmail.com>
2023-12-08 18:12:05 +0200

    add problim

--- 06.DOM-Events-Exercise/08. Sudomu/solution.js
+++ 06.DOM-Events-Exercise/08. Sudomu/solution.js
@@ -1,3 +1,21 @@
+/*
+8. Sudomu *
+
+Write a function that implements SUDOMU (Sudoku inside the DOM).
+The rules are simple and they are the same as the typical sudoku game
+If the table is filled with the right numbers, and the ["Quick Check"] button is clicked, the expected result should be:
+The table borer should be changed to: "2px solid green". The text content of the paragraph
+inside the div with an id "check" must be "You solve it! Congratulations!"
+The text color of that paragraph must be green.
+
+Otherwise, when the filled table does not solve the sudomu, the result should be:
+The table border should be changed to: "2px solid red". The text content of the paragraph inside the div
+with an id "check" must be: "NOP! You are not done yet..."
+The text color of that paragraph must be red!
+The ["Clear"] button clears the whole SUDOMU (removes all numbers) and the paragraph
+which contains the messages. It also removes the table border.
+*/
+
 function solve() {
 
     //TODO...
