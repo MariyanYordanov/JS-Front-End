4c64b56cc35ebe2daa3d94cd689e75a5b1088cf0
Mariyan Yordanov <marsrewq@gmail.com>
2023-12-13 01:46:28 +0200

    add solution

--- 07.HTTP-and-REST-Lab-Resources/05.Book-library/app.js
+++ 07.HTTP-and-REST-Lab-Resources/05.Book-library/app.js
@@ -46,27 +46,88 @@ By clicking the delete button you have to delete the book, without any confirmat
 To delete a book use "DELETE" command and send REQUEST: http://localhost:3030/jsonstore/collections/books/:id
 */
 
-function attachEvents() {
+async function attachEvents() {
+    const baseURL = 'http://localhost:3030/jsonstore/collections/books';
 
     const loadAllBooks = document.getElementById('loadBooks');
-    let bookTable = document.querySelector('tbody');
-    const bookList = document.querySelectorAll('tbody tr');
+    const bookTable = document.querySelector('tbody');
+    const formTitle = document.querySelector('input[name=title]');
+    const formAuthor = document.querySelector('input[name=author]');
 
-    loadAllBooks.addEventListener('click', () => {
-        fetch('http://localhost:3030/jsonstore/collections/books')
-            .then(res => res.json())
-            .then((data) => {
-                const books = Object.values(data)
-                
-                for (const book of books) {
-                    createBookRow(book);
-                }
-            });
+    const createBookButton = document.querySelector('form button');
+    const updateBookButton = document.createElement('button');
+    updateBookButton.textContent = 'Update';
+
+    loadAllBooks.addEventListener('click', async () => {
+        try {
+            const response = await fetch(baseURL);
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status} - ${response.statusText}`);
+            }
+            
+            const data = await response.json();
+            bookTable.innerHTML = '';
+            const books = Object.keys(data).reduce((acc, _id) => {
+                acc.push({ _id, ...data[_id] });
+                return acc;
+            }, []);
+            
+            books.forEach(book => createBookRow(book));
+        } catch (error) {
+            console.log(error);
+        }
     });
 
-    function createBookRow(book){
-        
+    updateBookButton.addEventListener('click', async (e) => {
+        e.preventDefault();
+
+        const book = {
+            title: formTitle.value,
+            author: formAuthor.value,
+            _id: bookId
+        };
+
+        try {
+            const response = await fetch(`${baseURL}/${book._id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(book)
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status} - ${response.statusText}`);
+            }
+
+            const data = await response.json();
+
+            const editRow = bookTable.querySelector('tr[data-update=true]');
+
+            const editTitle = editRow.firstChild;
+            const editAuthor = editTitle.nextSibling;
+
+            editTitle.textContent = book.title;
+            editAuthor.textContent = book.author;
+
+            buttonEdit.textContent = 'Edit';
+            updateBookButton.replaceWith(createBookButton);
+
+            row.removeAttribute('data-update');
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    let bookId; 
+
+    function createBookRow(book) {
         const row = document.createElement('tr');
+
+        const bookIdInput = document.createElement('input');
+        bookIdInput.type = 'hidden';
+        bookIdInput.value = book._id;
+        row.appendChild(bookIdInput);
 
         const dataTitle = document.createElement('td');
         dataTitle.textContent = book.title;
@@ -83,17 +144,94 @@ function attachEvents() {
         buttonDelete.textContent = 'Delete';
 
         bookTable.appendChild(row);
-
         row.appendChild(dataTitle);
         row.appendChild(dataAuthor);
         row.appendChild(dataButtons);
-
         dataButtons.appendChild(buttonEdit);
         dataButtons.appendChild(buttonDelete);
 
-        return row;
+        buttonEdit.addEventListener('click', async (e) => {
+            e.preventDefault();
+
+            if (buttonEdit.textContent === 'Cancel') {
+                formTitle.value = '';
+                formAuthor.value = '';
+
+                buttonEdit.textContent = 'Edit';
+                updateBookButton.replaceWith(createBookButton);
+
+                row.removeAttribute('data-update');
+            } else {
+                formTitle.value = book.title;
+                formAuthor.value = book.author;
+
+                buttonEdit.textContent = 'Cancel';
+                createBookButton.replaceWith(updateBookButton);
+
+                row.setAttribute('data-update', true);
+
+                bookId = book._id; 
+            }
+        });
+
+        buttonDelete.addEventListener('click', (e) => {
+            e.preventDefault();
+        
+            const rowToDelete = e.currentTarget.parentElement.parentElement;
+            const bookId = rowToDelete.querySelector('input[type="hidden"]').value;
+            const confirmation = window.confirm("Are you sure you want to delete this book?");
+            
+            if(confirmation){
+                fetch(`${baseURL}/${bookId}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then(res => {
+                    if (res.status !== 200) {
+                        throw new Error(`Error: ${res.status} ${res.statusText}`);
+                    }
+                    return res.json();
+                })
+                .then(rowToDelete.remove())
+                .catch(error => console.log('Error', error))
+            }
+            
+        });
     }
 
+    createBookButton.addEventListener('click', async (e) => {
+        e.preventDefault();
+
+        const book = {
+            title: formTitle.value,
+            author: formAuthor.value
+        };
+
+        try {
+            const response = await fetch(baseURL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(book)
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status} - ${response.statusText}`);
+            }
+
+            const result = await response.json();
+            const bookElement = createBookRow({ ...book, _id: result._id });
+            bookTable.appendChild(bookElement);
+
+            formTitle.value = '';
+            formAuthor.value = '';
+        } catch (error) {
+            console.log(error);
+        }
+    });
 }
 
 attachEvents();
