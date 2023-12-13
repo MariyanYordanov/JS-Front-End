56a1e1218ccea44d5c7221ca3a75b40a7b9af765
Mariyan Yordanov <marsrewq@gmail.com>
2023-12-13 22:56:43 +0200

    add new solution

--- JS Front-end Exam Preparation-1 Resources/03.Vacation Schedule/03.Vacation Schedule_Resources/app.js
+++ JS Front-end Exam Preparation-1 Resources/03.Vacation Schedule/03.Vacation Schedule_Resources/app.js
@@ -1,1 +1,18 @@
-//TODO..
+const baseURL = "http://localhost:3030/jsonstore/tasks";
+
+const loadVacationBtn = document.getElementById('load-vacations');
+loadVacationBtn.addEventListener('click', (e) => {
+    fetch(baseURL)
+        .then(res => res.json())
+        .then(data => console.log(Object.entries(data)))
+});
+
+/*
+<div class="container">
+    <h2>Carren Davis</h2>
+    <h3>2023-06-09</h3>
+    <h3>5</h3>
+    <button class="change-btn">Change</button>
+    <button class="done-btn">Done</button>
+</div>
+*/
